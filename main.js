5d3b5166b485a88828022b8741660e91f571a4af
Bayron Silva <bayronisraelsilva@gmail.com>
2023-12-01 17:42:25 -0300

    tercera pre-entrega2

---
+++ main.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// //*CARRITO
+// //ARRAY SERVICIOS
+// const servicios = [
+//     {
+//         nombre: "kinesiologia musculoesqueletica",
+//         precio: 35000,
+//     },
+//     {
+//         nombre: "neurorrehabilitacion adulto",
+//         precio: 35000,
+//     },
+//     {
+//         nombre: "kinesiologia respiratoria",
+//         precio: 35000,
+//     },
+//     {
+//         nombre: "masaje de reljacion",
+//         precio: 27000,
+//     },
+//     {
+//         nombre: "masaje descontracturante",
+//         precio: 33000,
+//     },
+//     {
+//         nombre: "masaje craneal",
+//         precio: 20000,
+//     },
+//     {
+//         nombre: "masaje terapeutico",
+//         precio: 35000,
+//     },
+//     {
+//         nombre: "recovery",
+//         precio: 45000,
+//     },
+// ]
+
+// let agregadosCarrito = []
+// let botonAgregar = document.querySelectorAll(".boton-item")
+// let contenedorServicios = document.getElementById("contenedor-servicios")
+
+// function recorridoServicios (arrayServicios) {
+//     arrayServicios.forEach(servicio => {
+        
+//     });
+//     funcionBoton ()
+// }
+// recorridoServicios (arrayServicios)
+
+// function funcionBoton () {
+//     botonAgregar = document.querySelectorAll(".boton-item")
+//     botonAgregar.foreach(button => {
+//         button.onclick = (e) => {
+//             const servicioId = e.currentTarget.id
+//             const seleccionServicio = servicios.find(servicio => ("#boton-id"))
+
+//             agregadosCarrito.push(seleccionServicio)
+//             console.log(agregadosCarrito)
+
+//         }
+//     })
+// }
